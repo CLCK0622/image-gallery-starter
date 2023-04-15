626a954a9d86f910ec40a2b9b4ca1c62cd4f4305
钟熠 <zhongyi070622@163.com>
2023-04-15 21:17:13 +0800

    edit framework

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from daily life of HSEFZ 2507."
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="2507 Album" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from daily life of HSEFZ 2507."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Image Gallery for HSEFZ 2507" />
+          <meta name="twitter:card" content="Image Gallery for HSEFZ 2507" />
+          <meta name="twitter:title" content="Image Gallery for HSEFZ 2507" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from daily life of HSEFZ 2507."
           />
         </Head>
         <body className="bg-black antialiased">
